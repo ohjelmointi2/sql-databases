5b16065e087d6a7d9fb68b4085a272dff4306f86
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-10-04 06:45:22 +0000

    Improved tests

--- src/test/java/databases/part03/AlbumDAOTest.java
+++ src/test/java/databases/part03/AlbumDAOTest.java
@@ -3,8 +3,12 @@ package databases.part03;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import databases.part02.Artist;
@@ -24,57 +28,103 @@ public class AlbumDAOTest {
      */
     private AlbumDAO albumDAO = new AlbumDAO(JDBC_URL);
 
+    private final Artist artist1 = new Artist(1, "AC/DC"); // artist 1 has 2 albums in test database
+    private final Artist artist3 = new Artist(3, "Artist 3"); // no albums in test database
+
     /**
-     * Ideally, each test should be independent of the others. This is usually
-     * accomplished by clearing the database before each test and by using a test
-     * fixture, so each test runs with the same initial data.
+     * Ideally, each test should be independent of the others. This is accomplished
+     * by clearing the database before each test and by using a test fixture, so
+     * each test runs with the same initial data.
      *
-     * Since we are using the same database for testing and development, we won't
-     * clear the database before each test. As a result, we add, change and delete
-     * albums in a single test. This also means that if one assertion fails, the
-     * rest of the test will fail, even if the other assertions would be correct.
-     *
-     * This is not ideal, but it is acceptable for this assignment.
+     * @throws SQLException
      */
+    @BeforeEach
+    void clearDatabase() throws SQLException {
+        Connection connection = DriverManager.getConnection(JDBC_URL);
+        connection.prepareStatement("DELETE FROM Album").executeUpdate();
+        connection
+                .prepareStatement(
+                        """
+                                INSERT INTO `Album` (`Title`, `ArtistId`) VALUES
+                                    ('Album 1 by artist 1', 1),
+                                    ('Album 2 by artist 1', 1),
+                                    ('Album 3 by artist 2', 2);
+                                """)
+                .executeUpdate();
+
+    }
+
     @Test
-    void testGettingAddingUpdatingAndDeletingAlbums() {
-        Artist artist = new Artist(1, "AC/DC");
+    void testGettingAlbumsForArtist() {
+        List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
 
-        // Get all albums by AC/DC and make sure there are more than 0:
-        List<Album> albums = albumDAO.getAlbumsByArtist(artist);
-        assertTrue(albums.size() > 0);
+        assertEquals(2, albums.size());
+        assertEquals("Album 1 by artist 1", albums.get(0).getTitle());
+        assertEquals("Album 2 by artist 1", albums.get(1).getTitle());
+    }
 
-        // The album's title contains quotes to verify the SQL query is properly
-        // escaped:
-        Album album = new Album("~~~ The album's \"name\"", 1);
+    @Test
+    void testGettingAlbumsForArtistWithNoAlbums() {
+        List<Album> albums = albumDAO.getAlbumsByArtist(artist3);
+
+        assertEquals(0, albums.size());
+    }
+
+    @Test
+    void testAddingAlbum() {
+        Album album = new Album("New Album", 1);
+
         boolean added = albumDAO.addAlbum(album);
         assertTrue(added);
 
-        // Make sure the number of albums increases by 1 and the last album is the one
-        // we just added:
-        List<Album> albumsAfterAdd = albumDAO.getAlbumsByArtist(artist);
-        Album latestAlbum = albums.get(albums.size() - 1);
-        assertEquals(albums.size() + 1, albumsAfterAdd.size());
-        assertEquals("~~~ The album's \"name\"", latestAlbum.getTitle(),
-                "The added album should be last in the list and the title should be properly escaped.");
-        assertEquals(1, latestAlbum.getArtistId());
+        List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
+        assertEquals(3, albums.size());
 
-        // Update the last album's title. Once again, the album's title contains quotes
-        // to test that the SQL query is properly escaped:
-        latestAlbum.setTitle("~~~ Updated \"Title\"");
+        Album latestAlbum = albums.get(albums.size() - 1);
+        assertEquals("New Album", latestAlbum.getTitle());
+    }
+
+    @Test
+    void testAddingAlbumWithSpecialCharacters() {
+        // The album's title contains characters that need to be escaped in SQL queries:
+        String title = "~~~ The album's \"name\"";
+
+        Album album = new Album(title, 1);
+
+        boolean added = albumDAO.addAlbum(album);
+        assertTrue(added);
+
+        List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
+        assertEquals(3, albums.size());
+
+        Album latestAlbum = albums.get(albums.size() - 1);
+        assertEquals(title, latestAlbum.getTitle());
+    }
+
+    @Test
+    void testUpdatingAlbums() {
+        List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
+        Album latestAlbum = albums.get(albums.size() - 1);
+        latestAlbum.setTitle("Updated Title");
+
         boolean updated = albumDAO.updateAlbum(latestAlbum);
         assertTrue(updated);
 
-        // Fetch the last album again and make sure the title was updated:
-        List<Album> albumsAfterUpdate = albumDAO.getAlbumsByArtist(artist);
+        List<Album> albumsAfterUpdate = albumDAO.getAlbumsByArtist(artist1);
         Album lastAfterUpdate = albumsAfterUpdate.get(albumsAfterUpdate.size() - 1);
-        assertEquals("~~~ Updated \"Title\"", lastAfterUpdate.getTitle());
+        assertEquals("Updated Title", lastAfterUpdate.getTitle());
+    }
 
-        // Finally, delete the last album and make sure the number of albums decreases:
+    @Test
+    void testDeletingAlbums() {
+        List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
+        assertEquals(2, albums.size());
+
+        Album latestAlbum = albums.get(albums.size() - 1);
         boolean deleted = albumDAO.deleteAlbum(latestAlbum);
         assertTrue(deleted);
 
-        List<Album> albumsAfterDelete = albumDAO.getAlbumsByArtist(artist);
-        assertEquals(albums.size(), albumsAfterDelete.size());
+        List<Album> albumsAfterDelete = albumDAO.getAlbumsByArtist(artist1);
+        assertEquals(1, albumsAfterDelete.size());
     }
 }
