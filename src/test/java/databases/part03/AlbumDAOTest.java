ed773d543a1a00248144d847eb95ef1100c2a694
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-11-27 10:26:43 +0200

    Make sure that the albums that are returned have ids.

--- src/test/java/databases/part03/AlbumDAOTest.java
+++ src/test/java/databases/part03/AlbumDAOTest.java
@@ -56,9 +56,20 @@ public class AlbumDAOTest {
     void testGettingAlbumsForArtist() {
         List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
 
+        // There should be 2 albums:
         assertEquals(2, albums.size());
+
+        // The titles should be correct:
         assertEquals("Album 1 by artist 1", albums.get(0).getTitle());
         assertEquals("Album 2 by artist 1", albums.get(1).getTitle());
+
+        // The AlbumIds should be correct:
+        assertEquals(1, albums.get(0).getId());
+        assertEquals(2, albums.get(1).getId());
+
+        // The ArtistIds should be correct:
+        assertEquals(1, albums.get(0).getArtistId());
+        assertEquals(1, albums.get(1).getArtistId());
     }
 
     @Test
@@ -70,14 +81,17 @@ public class AlbumDAOTest {
 
     @Test
     void testAddingAlbum() {
+        // A new album for artist 1:
         Album album = new Album("New Album", 1);
 
         boolean added = albumDAO.addAlbum(album);
         assertTrue(added);
 
+        // There should now be 3 albums for artist 1:
         List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
         assertEquals(3, albums.size());
 
+        // The new album should be the last one:
         Album latestAlbum = albums.get(albums.size() - 1);
         assertEquals("New Album", latestAlbum.getTitle());
     }
@@ -89,25 +103,33 @@ public class AlbumDAOTest {
 
         Album album = new Album(title, 1);
 
+        // The album should be added successfully:
         boolean added = albumDAO.addAlbum(album);
         assertTrue(added);
 
+        // There should now be 3 albums for artist 1:
         List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
         assertEquals(3, albums.size());
 
+        // The new album should be the last one, with the correct title:
         Album latestAlbum = albums.get(albums.size() - 1);
         assertEquals(title, latestAlbum.getTitle());
     }
 
     @Test
     void testUpdatingAlbums() {
+        // Get the albums for artist 1 before the update:
         List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
+
+        // Change the title of the last album:
         Album latestAlbum = albums.get(albums.size() - 1);
         latestAlbum.setTitle("Updated Title");
 
+        // Make sure the album was updated successfully:
         boolean updated = albumDAO.updateAlbum(latestAlbum);
         assertTrue(updated);
 
+        // Verify that the name of the last album was updated:
         List<Album> albumsAfterUpdate = albumDAO.getAlbumsByArtist(artist1);
         Album lastAfterUpdate = albumsAfterUpdate.get(albumsAfterUpdate.size() - 1);
         assertEquals("Updated Title", lastAfterUpdate.getTitle());
@@ -115,13 +137,16 @@ public class AlbumDAOTest {
 
     @Test
     void testDeletingAlbums() {
+        // Get the albums for artist 1 before the delete:
         List<Album> albums = albumDAO.getAlbumsByArtist(artist1);
         assertEquals(2, albums.size());
 
+        // Delete the last album:
         Album latestAlbum = albums.get(albums.size() - 1);
         boolean deleted = albumDAO.deleteAlbum(latestAlbum);
         assertTrue(deleted);
 
+        // Verify that the last album was deleted:
         List<Album> albumsAfterDelete = albumDAO.getAlbumsByArtist(artist1);
         assertEquals(1, albumsAfterDelete.size());
     }
